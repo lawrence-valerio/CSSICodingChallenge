dc875e5f330447aedef26ea1460d4652f934fecf
Lawrence Valerio <valerio.lawrence@gmail.com>
2022-05-10 13:48:21 -0500

    Created the electric car class.

--- ChallengeUI/Program.cs
+++ ChallengeUI/Program.cs
@@ -43,5 +43,20 @@ namespace ChallengeUI
                 throw new NotImplementedException();
             }
         }
+
+        public class ElectricCar : Vehicle, IVehicle
+        {
+            public string MotorType { get; set; }
+
+            public string Color { get; set; }
+            public string Manufacturer { get; set; }
+            public string Model { get; set; }
+            public int YearBuilt { get; set; }
+
+            public int Calculate()
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
