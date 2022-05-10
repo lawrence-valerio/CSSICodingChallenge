13a681d526f5c74f75b82b0e9685b74d927419eb
Lawrence Valerio <valerio.lawrence@gmail.com>
2022-05-10 14:09:15 -0500

    Added Model and manufacturer enum.

---
+++ ChallengeLibrary/Enums/Manufacturer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeLibrary.Enums
+{
+    public enum Manufacturer
+    {
+        Toyota,
+        Volkswagen,
+        Honda,
+        Ford,
+        BMW,
+        Hyundai
+    }
+}
---
+++ ChallengeLibrary/Enums/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeLibrary.Enums
+{
+    public enum Model
+    {
+        Accent,
+        Elantra,
+        Accord,
+        Odyssey,
+        Pilot,
+        Edge,
+        Escape
+    }
+}
