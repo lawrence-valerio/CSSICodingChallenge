aa63877c2b4a86aba8175596022dcbe7b6d14cc8
Lawrence Valerio <valerio.lawrence@gmail.com>
2022-05-10 14:01:00 -0500

    Added class library to the solution.

---
+++ ChallengeLibrary/Enums/Color.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeLibrary.Enums
+{
+    public enum Color
+    {
+        Black,
+        Red,
+        Green,
+        Blue,
+        Yellow
+    }
+}
